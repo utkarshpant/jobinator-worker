0e07cdc12ed378714cc421e308cd611761a3f64a
Utkarsh Pant <utkarsh.pant@outlook.com>
2024-09-21 15:51:38 -0500

    Working resume analysis

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import openai, { OpenAI } from 'openai';
+
+interface EnvVars {
+	OPENAI_API_KEY: string;
+	JOBINATOR: KVNamespace;
+}
+
+interface Env extends EnvVars {}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env: Env, ctx): Promise<Response> {
+		const openai = new OpenAI({
+			apiKey: env.OPENAI_API_KEY,
+		});
+		const resume = await env.JOBINATOR.get('resume');
+		console.log({
+			role: 'user',
+			content: `Resume:\n\n${resume}`
+		});
+		const modelResponse = await openai.chat.completions.create({
+			model: 'gpt-4o-mini',
+			messages: [
+				{
+					role: 'system',
+					content: "You're a knowledgeable recruiter in the tech industry in the US. The user is a software engineer looking for a job and would like a resume review. The resume is provided in stringified JSON - please analyse it and rate it from 1 to 10, and provide feedback for improvement. Since the input is stringified JSON, offer a qualitative analysis of the resume's content and structure only."
+				},
+				{
+					role: 'user',
+					content: `Resume:\n\n${resume}`
+				}
+			]
+		})
+
+		return new Response(modelResponse.choices[0].message.content);
 	},
 } satisfies ExportedHandler<Env>;
