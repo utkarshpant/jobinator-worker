e416d205ca6832f2dc0d5a01d430c914330812aa
Utkarsh Pant <utkarsh.pant@outlook.com>
2024-09-24 10:41:24 -0500

    Update worker

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const getJobDescriptionFromUrl = async (url: URL) => {
 					return $(this).children('div').length === 3;
 				})
 				.text();
-		default:	
+		default:
 			return $('.job__description').text();
 	}
 };
@@ -50,49 +50,52 @@ interface Env extends EnvVars {}
 export default {
 	async fetch(request, env: Env, ctx): Promise<Response> {
 		const requestURL = new URL(request.url);
-		if (!requestURL.searchParams.has('url')) {
-			return new Response('Hello World!');
+
+		if (requestURL.pathname === '/recommendations') {
+			if (!requestURL.searchParams.has('url')) {
+				return new Response('Hello World!');
+			}
+			const jobDescription = await getJobDescriptionFromUrl(new URL(decodeURIComponent(requestURL.searchParams.get('url') as string)));
+			const openai = new OpenAI({
+				apiKey: env.OPENAI_API_KEY,
+			});
+			const resume = await env.JOBINATOR.get('resume');
+			const everything = await env.JOBINATOR.get('everything');
+
+			const modelResponse = await openai.chat.completions.create({
+				model: 'gpt-4o-mini',
+				messages: [
+					{
+						role: 'system',
+						content:
+							"Your task is to suggest additions or substitutions *for each section* in the user's resume, picked from the longer reference document *only*, in order to align the resume with the job application. \
+							For each section of the resume, please provide a bulleted list of additions, followed by substitutions, if any, and don't repeat a suggestion. Please don't change the contents of any bullet points! \
+							You may also provide a brief explanation for each suggestion in parentheses in each bullet point. In a final section, suggest improvements to grammar/framing/action verbs, etc.",
+					},
+					{
+						role: 'user',
+						content: `\n\nResume:\n\n${resume}`,
+					},
+					{
+						role: 'user',
+						content: `\n\nJob Description:\n\n${jobDescription}`,
+					},
+					{
+						role: 'user',
+						content: `\n\nReference Document:\n\n${everything}`,
+					},
+				],
+				stream: false,
+			});
+			const modelResponseHTML = await markdownToHTML(modelResponse.choices[0].message.content as string);
+
+			return new Response(modelResponseHTML, {
+				headers: {
+					'Content-Type': 'text/html',
+				},
+			});
 		}
-		const jobDescription = await getJobDescriptionFromUrl(new URL(decodeURIComponent(requestURL.searchParams.get('url') as string)));
-		const openai = new OpenAI({
-			apiKey: env.OPENAI_API_KEY,
-		});
-		const resume = await env.JOBINATOR.get('resume');
-		const everything = await env.JOBINATOR.get('everything');
 
-		const modelResponse = await openai.chat.completions.create({
-			model: 'gpt-4o-mini',
-			messages: [
-				{
-					role: 'system',
-					content:
-						"You're a knowledgeable recruiter in the tech industry in the US, where the user is applying for jobs. \
-					Your task is to suggest additions or substitutions *for each section* in the user's resume, picked from the longer reference document *only*, in order to align the resume with the job application. \
-					Both the resume and the reference document have been parsed into valid JSON, so assume that the camelCase is a JSON artifact. \
-					For each section of the resume, please provide a bulleted list of additions, followed by substitutions, if any, and don't repeat a suggestion. Please don't change the contents of any bullet points! \
-					You may also provide a brief explanation for each suggestion in parentheses in each bullet point. In a final section, suggest improvements to grammar/framing/action verbs, etc.",
-				},
-				{
-					role: 'user',
-					content: `\n\nResume:\n\n${resume}`,
-				},
-				{
-					role: 'user',
-					content: `\n\nJob Description:\n\n${jobDescription}`,
-				},
-				{
-					role: 'user',
-					content: `\n\nReference Document:\n\n${everything}`,
-				},
-			],
-		});
-
-		const modelResponseHTML = await markdownToHTML(modelResponse.choices[0].message.content as string);
-
-		return new Response(modelResponseHTML, {
-			headers: {
-				'Content-Type': 'text/html',
-			},
-		});
+		return new Response('Hello, World!', { status: 200 });
 	},
 } satisfies ExportedHandler<Env>;
